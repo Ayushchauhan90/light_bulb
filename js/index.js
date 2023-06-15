288f4c015fadacc7d4a82f5628b9541708598ab5
Ayush Chauhan <aayuchauhan132@gmail.com>
2023-06-15 16:04:36 +0530

    first message

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+function bulb_on(){
+    document.getElementById("bulb-img").src='https://i.postimg.cc/6QyTynzr/bulb-on.png'
+}
+function bulb_of(){
+    document.getElementById("bulb-img").src='https://i.postimg.cc/KjK1wL3c/bulb-off.png'
+}
